6cb770b4410ae86449d51be530b328bb6fbf82db
jeonseungbeom <7237sbj@gmail.com>
2023-05-03 23:10:40 +0900

    Modify: 배송지 입력란, 결제제품  컴포넌트화

--- src/pages/Payment/Payment.jsx
+++ src/pages/Payment/Payment.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import PaymentProduct from './component/PaymentProduct';
+import ShippingAddress from './component/ShippingAddress';
 import './Payment.scss';
 
 const Payment = () => {
@@ -7,48 +9,7 @@ const Payment = () => {
       <h1 className="payment-title">결제하기</h1>
       <div className="payment-container">
         <div className="payment-step">
-          <section className="shipping-address">
-            <h2 className="shipping-address-title">배송 옵션</h2>
-            <div>
-              <input className="same-as-orderer" type="checkbox" />
-              <label>주문자 정보와 동일</label>
-            </div>
-            <div>
-              <input className="input-last-name" type="text" placeholder="성" />
-              <input
-                className="input-first-name"
-                type="text"
-                placeholder="이름"
-              />
-            </div>
-            <div className="hint-name">
-              <span className="hint-last-name">성을 입력하세요</span>
-              <span className="hint-first-name">이름을 입력하세요.</span>
-            </div>
-            <input
-              className="input-address"
-              type="text"
-              placeholder="도로명, 건물명 또는 지번으로 검색 예) 테헤란로 152, 혹은 역삼동
-              737"
-            />
-            <div className="hint-address">
-              주소 선택을 완료하거나 수동으로 주소를 입력해 주세요.
-            </div>
-            <div className="shipper-information">
-              <input
-                className="input-phone-number"
-                type="text"
-                placeholder="전화번호"
-              />
-              <input className="input-email" type="text" placeholder="이메일" />
-            </div>
-            <div className="hint-information">
-              <span className="hint-phone-number">필수 작성 항목입니다.</span>
-              <span className="hint-email">
-                유효한 이메일 주소를 입력하세요.
-              </span>
-            </div>
-          </section>
+          <ShippingAddress />
           <section className="payment-progress">
             <h2 className="payment-progress-title">결제</h2>
             <div className="payment-point-current">
@@ -87,24 +48,7 @@ const Payment = () => {
           <div className="payment-list-title">구매 목록</div>
           <ul>
             {[1, 2, 3, 4].map(item => (
-              <li key={item.index} className="payment-product">
-                <img
-                  className="payment-product-img"
-                  src="/images/payment/sampling.png"
-                  alt="이미지가 없습니다."
-                />
-                <div className="payment-product-information">
-                  <div>중국의 짜장밥</div>
-                  <div className="payment-product-name-eng">
-                    blackrice of china
-                  </div>
-                  <div className="payment-product-region">중국 / 아시아</div>
-                  <div className="payment-product-quantity-price">
-                    수량 : 3개 / 30,000원
-                  </div>
-                  <div className="payment-product-total-price">150,000원</div>
-                </div>
-              </li>
+              <PaymentProduct key={item.index} />
             ))}
           </ul>
         </section>
---
+++ src/pages/Payment/component/ShippingAddress.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import './ShippingAddress.scss';
+
+const ShippingAddress = () => {
+  return (
+    <section className="shipping-address">
+      <h2 className="shipping-address-title">배송 옵션</h2>
+      <div>
+        <input className="same-as-orderer" type="checkbox" />
+        <label>주문자 정보와 동일</label>
+      </div>
+      <div>
+        <input className="input-last-name" type="text" placeholder="성" />
+        <input className="input-first-name" type="text" placeholder="이름" />
+      </div>
+      <div className="hint-name">
+        <span className="hint-last-name">성을 입력하세요</span>
+        <span className="hint-first-name">이름을 입력하세요.</span>
+      </div>
+      <input
+        className="input-address"
+        type="text"
+        placeholder="도로명, 건물명 또는 지번으로 검색 예) 테헤란로 152, 혹은 역삼동
+              737"
+      />
+      <div className="hint-address">
+        주소 선택을 완료하거나 수동으로 주소를 입력해 주세요.
+      </div>
+      <div className="shipper-information">
+        <input
+          className="input-phone-number"
+          type="text"
+          placeholder="전화번호"
+        />
+        <input className="input-email" type="text" placeholder="이메일" />
+      </div>
+      <div className="hint-information">
+        <span className="hint-phone-number">필수 작성 항목입니다.</span>
+        <span className="hint-email">유효한 이메일 주소를 입력하세요.</span>
+      </div>
+    </section>
+  );
+};
+
+export default ShippingAddress;
