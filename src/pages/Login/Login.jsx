4a521c7fb7836c72f527d27c07bd4f4b176aa6c0
min2oyo <min2u@outlook.com>
2023-05-03 14:32:07 +0900

    Add: 로그인 레이아웃

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import './Login.scss';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   return (
     <div className="login">
-      <div className="container">Login</div>
+      <div className="container">
+        <span className="text1">Enter your email to join us or sign in.</span>
+        <form action="#" onSubmit={e => e.preventDefault()}>
+          <input type="email" className="input-email" placeholder="Email" />
+          <div className="email-required" hidden>
+            Required
+          </div>
+          <input
+            type="password"
+            className="input-password"
+            placeholder="password"
+          />
+          <div className="text2" hidden>
+            By continuing, I agree to Seke’s Privacy Policy.
+          </div>
+          <Link to="#">
+            <div className="text3">Forgot password?</div>
+          </Link>
+          <button>Continue</button>
+          <button>Sign In</button>
+        </form>
+      </div>
     </div>
   );
 };
