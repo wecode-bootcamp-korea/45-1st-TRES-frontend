add4e60ed7e53785b951d6fd666856310bd5f776
taewon kim <taewon2659@gmail.com>
2023-05-04 15:44:27 +0900

    Modify: footer,main 컴포넌트 내용 삭제

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,11 +2,7 @@ import React from 'react';
 import './Footer.scss';
 
 const Footer = () => {
-  return (
-    <div className="footer">
-      <div className="footer-box">Footer</div>
-    </div>
-  );
+  return;
 };
 
 export default Footer;
