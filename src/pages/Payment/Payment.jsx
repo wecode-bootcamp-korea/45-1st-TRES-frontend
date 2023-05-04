93ef6bebe0db32db1ce630b7506b27d86927b980
jeonseungbeom <7237sbj@gmail.com>
2023-05-04 23:12:38 +0900

    Refactor: 코드 리팩토링 (멘토 리뷰 반영)

--- src/pages/Payment/Payment.jsx
+++ src/pages/Payment/Payment.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PaymentProduct from './component/PaymentProduct';
 import ShippingAddress from './component/ShippingAddress';
 import './Payment.scss';
 
 const Payment = () => {
+  const [paymentPaymentProductList, setPaymentProductList] = useState([]);
+
+  useEffect(() => {
+    fetch('/data/paymentProductData.json', {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(data => {
+        setPaymentProductList(data);
+      });
+  }, []);
+
   return (
     <div className="payment">
       <h1 className="payment-title">결제하기</h1>
@@ -12,34 +24,38 @@ const Payment = () => {
           <ShippingAddress />
           <section className="payment-progress">
             <h2 className="payment-progress-title">결제</h2>
-            <div className="payment-point-current">
+            <div className="payment-calculate">
               <span>보유 포인트</span>
               <span>100,000원</span>
             </div>
-            <div className="payment-price">
+            <div className="payment-calculate">
               <span>상품 금액</span>
               <span>78,000원</span>
             </div>
-            <div className="payment-delivery-price">
+            <div className="payment-calculate">
               <span>배송비</span>
               <span>3,000원</span>
             </div>
-            <div className="payment-total-price">
+            <div className="payment-calculate">
               <span>총 결제 금액</span>
               <span>81,000원</span>
             </div>
-            <div className="payment-point-remain">
+            <div className="payment-calculate">
               <span>남은 포인트</span>
               <span>19,000원</span>
             </div>
           </section>
           <section className="payment-complete">
             <h2 className="payment-complete-title">주문 완료</h2>
-            <div>
-              <input className="consent-to-purchase" type="checkbox" />
-              <label>구매 약관에 동의합니다.</label>
-              <div>
-                <button className="payment-complete-button">주문하기</button>
+            <div className="purchase">
+              <input
+                id="check-box-consent"
+                className="consent-to-purchase"
+                type="checkbox"
+              />
+              <label htmlFor="check-box-consent">구매 약관에 동의합니다.</label>
+              <div className="purchase-complete-button">
+                <button className="purchase-button">주문하기</button>
               </div>
             </div>
           </section>
@@ -47,8 +63,8 @@ const Payment = () => {
         <section className="payment-list">
           <div className="payment-list-title">구매 목록</div>
           <ul>
-            {[1, 2, 3, 4].map(item => (
-              <PaymentProduct key={item.index} />
+            {paymentPaymentProductList.map(item => (
+              <PaymentProduct key={item.id} />
             ))}
           </ul>
         </section>
