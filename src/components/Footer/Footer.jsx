c2fbadcface8a6820b7bde58846c8f6f95490efb
taewon kim <taewon2659@gmail.com>
2023-05-04 15:00:13 +0900

    footer layout 제작 중

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import FooterMain from './component/FooterMain/FooterMain';
 import './Footer.scss';
 
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="footer-box">Footer</div>
+      <div className="footer-container">
+        <FooterMain />
+        <div className="footer-sub-section">d</div>
+        <div className="footer-legal-section">d</div>
+      </div>
     </div>
   );
 };
