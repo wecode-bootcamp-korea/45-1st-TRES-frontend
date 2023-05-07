4d594e9416df1c24e05eb46de86d58070badc7a8
min2oyo <min2u@outlook.com>
2023-05-07 19:57:31 +0900

    Add: nested router 적용

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,32 +1,28 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Nav from './components/Nav/Nav';
+import Container from './components/Container/Container';
 import Main from './pages/Main/Main';
-import LoginTemporary from './pages/Login/LoginTemporary';
 import Login from './pages/Login/Login';
 import ProductList from './pages/ProductList/ProductList';
 import ProductDetail from './pages/ProductDetail/ProductDetail';
 import Cart from './pages/Cart/Cart';
 import Payment from './pages/Payment/Payment';
 import Empty from './pages/Empty/Empty';
-import Footer from './components/Footer/Footer';
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Nav />
       <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/join" element={<Login />} />
-        {/* <Route path="/login" element={<Login />} /> */}
-        <Route path="/login" element={<LoginTemporary />} />
-        <Route path="/product-list" element={<ProductList />} />
-        <Route path="/product-detail" element={<ProductDetail />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/payment" element={<Payment />} />
+        <Route element={<Container />}>
+          <Route path="/" element={<Main />} />
+          <Route path="/product-list" element={<ProductList />} />
+          <Route path="/product-detail" element={<ProductDetail />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/payment" element={<Payment />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
         <Route path="*" element={<Empty />} />
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
 };
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Login.scss';
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState(``);
+  const [password, setPassword] = useState(``);
+
+  const inputEmail = e => {
+    setEmail(e.target.value);
+  };
+
+  const inputPassword = e => {
+    setPassword(e.target.value);
+  };
+
+  const login = () => {
+    fetch('http://10.58.52.191:3000/users/login', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    })
+      .then(res => {
+        if (res.ok) res.json();
+        throw new Error('통신실패!');
+      })
+      .catch(err => alert(`로그인 실패 ${err}`))
+      .then(res => {
+        localStorage.setItem('TOKEN', res.accessToken);
+        navigate('/');
+      });
+  };
+
   return (
     <div className="login">
-      <div className="container">Login</div>
+      <div className="container">
+        <span className="login-text">로그인</span>
+
+        <form className="form" action="#" onSubmit={login}>
+          <input
+            type="text"
+            className="input email"
+            name="email"
+            value={email}
+            placeholder="Email"
+            onChange={inputEmail}
+          />
+
+          <input
+            type="password"
+            className="input password"
+            value={password}
+            placeholder="Password"
+            onChange={inputPassword}
+          />
+
+          <button className="submit-button">로그인</button>
+        </form>
+      </div>
     </div>
   );
 };
