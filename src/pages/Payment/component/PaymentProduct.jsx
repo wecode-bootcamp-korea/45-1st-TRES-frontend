4ff89bfa51848f542044b1c1fbe8c3ecfea6f97e
jeonseungbeom <7237sbj@gmail.com>
2023-05-07 22:18:49 +0900

    Add: 결제 구매 목록 구현

--- src/pages/Payment/component/PaymentProduct.jsx
+++ src/pages/Payment/component/PaymentProduct.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import './PaymentProduct.scss';
 
-const PaymentProduct = () => {
+const PaymentProduct = ({ item }) => {
+  const {
+    foodName,
+    foodNameEng,
+    country,
+    continent,
+    price,
+    quantity,
+    foodImg,
+  } = item;
+  const priceSum = price * quantity;
   return (
     <li className="payment-product">
-      <img
-        className="payment-product-img"
-        src="/images/payment/sampling.png"
-        alt="이미지가 없습니다."
-      />
+      <img className="payment-product-img" src={foodImg} alt="음식사진" />
       <div className="payment-product-information">
-        <div>중국의 짜장밥</div>
-        <div className="payment-product name-eng">blackrice of china</div>
-        <div className="payment-product region">중국 / 아시아</div>
+        <div>{foodName}</div>
+        <div className="payment-product name-eng">{foodNameEng}</div>
+        <div className="payment-product region">{`${country} / ${continent}`}</div>
         <div className="payment-product quantity-price">
-          수량 : 3개 / 30,000원
+          {`${quantity} / ${price}`}
         </div>
-        <div className="payment-product total-price">150,000원</div>
+        <div className="payment-product total-price">{priceSum}원</div>
       </div>
     </li>
   );
