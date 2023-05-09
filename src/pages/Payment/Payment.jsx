b24af2421189472feb4df68052f0ef4e37e2d223
jeonseungbeom <7237sbj@gmail.com>
2023-05-09 10:21:48 +0900

    Add: 주문완료 모달창 구현

--- src/pages/Payment/Payment.jsx
+++ src/pages/Payment/Payment.jsx
@@ -1,33 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import PaymentProduct from './component/PaymentProduct';
 import ShippingAddress from './component/ShippingAddress';
+import PaymentModal from './component/PaymentModal';
 import './Payment.scss';
 
 const Payment = () => {
-  const [PaymentProductList, setPaymentProductList] = useState([]);
-  const [isDisabledPayment, setIsDisabledPayment] = useState(true);
-  const [isCheckedTrems, setIsCheckedTrems] = useState(false);
-  const possessionPoint = 30000;
-  const paymentPrice = 20000;
-  const remainPoint = possessionPoint - paymentPrice;
-  const handleTermsOfPurchase = () => {
-    if (isCheckedTrems) {
-      setIsCheckedTrems(false);
-      setIsDisabledPayment(true);
-    } else {
-      if (possessionPoint >= paymentPrice) {
-        setIsCheckedTrems(true);
-        setIsDisabledPayment(false);
-      }
-    }
+  const [foodList, setFoodList] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const showModal = () => {
+    setModalOpen(true);
   };
+
   useEffect(() => {
-    fetch('http://localhost:3000/data/paymentProductData.json', {
+    fetch('/data/paymentData.json', {
       method: 'GET',
     })
       .then(res => res.json())
       .then(data => {
-        setPaymentProductList(data);
+        setFoodList(data[0].food);
       });
   }, []);
 
@@ -41,23 +32,23 @@ const Payment = () => {
             <h2 className="payment-progress-title">결제</h2>
             <div className="payment-calculate">
               <span>보유 포인트</span>
-              <span>{possessionPoint.toLocaleString()}원</span>
+              <span>100,000원</span>
             </div>
             <div className="payment-calculate">
               <span>상품 금액</span>
-              <span>-20,000원</span>
+              <span>78,000원</span>
             </div>
             <div className="payment-calculate">
               <span>배송비</span>
-              <span>-0원</span>
+              <span>3,000원</span>
             </div>
             <div className="payment-calculate">
               <span>총 결제 금액</span>
-              <span>{paymentPrice.toLocaleString()}원</span>
+              <span>81,000원</span>
             </div>
             <div className="payment-calculate">
               <span>남은 포인트</span>
-              <span>{remainPoint.toLocaleString()}원</span>
+              <span>19,000원</span>
             </div>
           </section>
           <section className="payment-complete">
@@ -67,17 +58,18 @@ const Payment = () => {
                 id="check-box-consent"
                 className="consent-to-purchase"
                 type="checkbox"
-                onChange={handleTermsOfPurchase}
-                checked={isCheckedTrems}
               />
               <label htmlFor="check-box-consent">구매 약관에 동의합니다.</label>
               <div className="purchase-complete-button">
-                <button
-                  className="purchase-button"
-                  disabled={isDisabledPayment}
-                >
+                <button className="purchase-button" onClick={showModal}>
                   주문하기
                 </button>
+                {modalOpen && (
+                  <PaymentModal
+                    setModalOpen={setModalOpen}
+                    foodList={foodList}
+                  />
+                )}
               </div>
             </div>
           </section>
@@ -85,8 +77,8 @@ const Payment = () => {
         <section className="payment-list">
           <div className="payment-list-title">구매 목록</div>
           <ul>
-            {PaymentProductList.map(item => (
-              <PaymentProduct key={item.id} />
+            {foodList.map(item => (
+              <PaymentProduct key={item.foodId} item={item} />
             ))}
           </ul>
         </section>
