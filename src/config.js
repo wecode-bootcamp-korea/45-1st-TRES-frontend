9b8a42392b057a59121df2aaad17f6d4755d6362
min2oyo <min2u@outlook.com>
2023-05-11 15:15:51 +0900

    fix: 회원가입 api 주소 변경

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ const BASE_URL = 'http://10.58.52.249:3000';
 
 export const EMAIL_VERIFICATION_API = `${BASE_URL}/users/email-check`;
 export const LOGIN_API = `${BASE_URL}/users/login`;
-export const JOIN_API = `${BASE_URL}/users/signup`;
+export const JOIN_API = `${BASE_URL}/users/`;
 export const COUNTRIES_API = `${BASE_URL}/users`;
