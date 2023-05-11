94d965d5d5af0e1c6c7980cefecbc4db8f42db4f
joyfuldev <wings2132@naver.com>
2023-05-11 22:18:25 +0900

    Modify: url 변경

--- src/pages/ProductList/component/Product.jsx
+++ src/pages/ProductList/component/Product.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 const Product = ({ product: { id, food, eng_food, price, food_image } }) => {
   const navigate = useNavigate();
   return (
-    <div className="product" onClick={() => navigate(`/product-detail/${id}`)}>
+    <div className="product" onClick={() => navigate(`/products/${id}`)}>
       <img className="food-img" alt="음식 사진" src={food_image} />
       <p className="korean-name">{food}</p>
       <p className="english-name">{eng_food}</p>
